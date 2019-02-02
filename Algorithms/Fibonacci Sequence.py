5330ff7bbe26bced124b3f908ec789fef4fc7f6b
dpaniq <14brother@gmail.com>
2019-02-02 20:59:08 +0300

    +

---
+++ Algorithms/Fibonacci Sequence.py
@@ -0,0 +1,24 @@
+# My simple option with limited output
+# def F(n):
+#      # return a list of fibonacci numbers
+#      lis = [0,1]
+#      for i in range(2,n):
+#          lis.append(lis[i-2] + lis[i-1])
+#      return(lis[0:n])
+#
+# F(n)
+
+# But better
+
+# def F(n):
+#     if n == 0: return 0
+#     elif n == 1: return 1
+#     else: return F(n-1)+F(n-2)
+
+# Generator
+
+def F():
+    a,b = 0,1
+    while True:
+        yield a
+        a, b = b, a + b
